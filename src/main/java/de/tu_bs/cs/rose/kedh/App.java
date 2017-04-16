3d0a712de4821342e5a91eb8954b21a7c686ea01
Jewgeni Rose <j.rose@tu-bs.de>
2017-04-16 11:00:06 +0200

    foo

--- src/main/java/de/tu_bs/cs/rose/kedh/App.java
+++ src/main/java/de/tu_bs/cs/rose/kedh/App.java
@@ -1,38 +1,37 @@
 package de.tu_bs.cs.rose.kedh;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.log4j.BasicConfigurator;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.common.SolrInputDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class App {
-
-  private static final Logger logger = LoggerFactory.getLogger(App.class);
-
-  private static SolrInputDocument buildDocument(final Map<String, String> fields) {
-    final SolrInputDocument doc = new SolrInputDocument();
-    fields.forEach((name, value) -> doc.addField(name, value));
-    return doc;
-  }
-
-  public static void main(final String[] args) throws SolrServerException, IOException {
-    BasicConfigurator.configure();
-    logger.info("Hello World");
-
-    final String url = "http://localhost:8983/solr/kedh";
-
-    final SolrConnector solr = new SolrConnector(url);
-
-    final Map<String, String> fields = new HashMap<>();
-    fields.put("id", "42");
-    fields.put("name", "Jewgeni Rose");
-    final SolrInputDocument doc = buildDocument(fields);
-
-    solr.index(doc);
-  }
+    
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+    
+    private static SolrInputDocument buildDocument(final Map<String, String> fields) {
+        final SolrInputDocument doc = new SolrInputDocument();
+        fields.forEach((name, value) -> doc.addField(name, value));
+        return doc;
+    }
+    
+    public static void main(final String[] args) throws SolrServerException, IOException {
+        BasicConfigurator.configure();
+        
+        final String url = "http://localhost:8983/solr/kedh";
+        
+        final SolrConnector solr = new SolrConnector(url);
+        
+        final Map<String, String> fields = new HashMap<>();
+        fields.put("id", "42");
+        fields.put("name", "Jewgeni Rose");
+        final SolrInputDocument doc = buildDocument(fields);
+        
+        solr.index(doc);
+    }
 }
--- src/main/java/de/tu_bs/cs/rose/kedh/SolrConnector.java
+++ src/main/java/de/tu_bs/cs/rose/kedh/SolrConnector.java
@@ -1,32 +1,32 @@
 package de.tu_bs.cs.rose.kedh;
 
-import java.io.IOException;
-
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.common.SolrInputDocument;
 
+import java.io.IOException;
+
 public class SolrConnector {
-
-	private final String url;
-	private final SolrClient client;
-
-	public SolrConnector(final String url) {
-		this.url = url;
-		this.client = new HttpSolrClient.Builder(url).build();
-	}
-
-	public String getURL() {
-		return url;
-	}
-
-	public SolrClient getClient() {
-		return client;
-	}
-
-	public void index(final SolrInputDocument document) throws SolrServerException, IOException {
-		client.add(document);
-		client.commit();
-	}
+    
+    private final String url;
+    private final SolrClient client;
+    
+    public SolrConnector(final String url) {
+        this.url = url;
+        this.client = new HttpSolrClient.Builder(url).build();
+    }
+    
+    public String getURL() {
+        return url;
+    }
+    
+    public SolrClient getClient() {
+        return client;
+    }
+    
+    public void index(final SolrInputDocument document) throws SolrServerException, IOException {
+        client.add(document);
+        client.commit();
+    }
 }
